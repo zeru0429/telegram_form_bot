ddfe35fd1e344b259b636ec33e03ce64a85462fa
zeru0429 <mihiretutigistu@gmail.com>
2023-06-03 17:09:58 +0300

    last bot edition

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, Conve
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
 # Bot states
-NAME, PHOTO, EMAIL, PHONE_NUMBER, ADDITIONAL_PHOTOS = range(5)
+NAME, EMAIL, PHONE_NUMBER, ADDITIONAL_PHOTOS = range(4)
 
 # MySQL database configuration
 db_config = {
@@ -18,11 +18,18 @@ db_config = {
 }
 
 count = 1
+user_name = ""
+user_email = ""
+user_phone_number = ""
+
+
 # Start command handler
 def start(update: Update, context) -> int:
+    global count
+    count = 1
     reply_keyboard = [['Cancel']]
     update.message.reply_text(
-        "Welcome to Begize Salon!\n Please enter your organization name",
+        "እንኳን ወደ በጊዜ ሳሎን መመዝገቢያ ቦት መጡ! \n እባኮን የድርጅቶን ስም ያስገቡ",
         reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
     )
     return NAME
@@ -32,20 +39,7 @@ def start(update: Update, context) -> int:
 def name_handler(update: Update, context) -> int:
     user_name = update.message.text
     context.user_data['name'] = user_name
-    update.message.reply_text("Your organization name: {}".format(user_name))
-    update.message.reply_text("Please insert your license scanned photo.")
-    return PHOTO
-
-
-# Photo handler
-def photo_handler(update: Update, context) -> int:
-    user_photo = update.message.photo[-1].get_file()
-    user_photo.download('{}_photo.jpg'.format(update.message.from_user.id))
-    update.message.reply_text("Your license:")
-    context.bot.send_photo(chat_id=update.message.chat_id,
-                           photo=open('{}_photo.jpg'.format(update.message.from_user.id), 'rb'))
-    update.message.reply_text("Please enter your email address.")
-
+    update.message.reply_text("እባኮን የድርጅቶን email አድራሻ ያስገቡ")
     return EMAIL
 
 
@@ -53,9 +47,7 @@ def photo_handler(update: Update, context) -> int:
 def email_handler(update: Update, context) -> int:
     user_email = update.message.text
     context.user_data['email'] = user_email
-    update.message.reply_text("Your email: {}".format(user_email))
-    update.message.reply_text("Please enter your phone number.")
-
+    update.message.reply_text("እባኮን የድርጅቶን ስልክ ቁጥር ያስገቡ")
     return PHONE_NUMBER
 
 
@@ -63,12 +55,13 @@ def email_handler(update: Update, context) -> int:
 def phone_number_handler(update: Update, context) -> int:
     user_phone_number = update.message.text
     context.user_data['phone_number'] = user_phone_number
-    update.message.reply_text("Your phone number: {}".format(user_phone_number))
-    update.message.reply_text("Please send photos for your organization profile. Send 'Done' when you finish.")
+    update.message.reply_text("እባኮን ለድርጅቶን ፕሮፋይል የሚሆኑ ፖቶ ይላኩ። \n ሲጨርሱ Done ብለዉ ይላኩ ")
+
     return ADDITIONAL_PHOTOS
 
 
 # Additional photos handler
+
 def additional_photos_handler(update: Update, context) -> int:
     global count
     if 'additional_photos' not in context.user_data:
@@ -82,14 +75,18 @@ def additional_photos_handler(update: Update, context) -> int:
         context.user_data['additional_photos'].append(
             '{}_additional_photo_{}.jpg'.format(update.message.from_user.id,
                                                 len(context.user_data['additional_photos']) + 1))
-        update.message.reply_text(f'Profile photo {count}:')
-        count+=1
-        context.bot.send_photo(chat_id=update.message.chat_id,
-                               photo=open('{}_additional_photo_{}.jpg'.format(update.message.from_user.id,
-                                                                              len(context.user_data[
-                                                                                      'additional_photos'])), 'rb'))
+
     elif update.message.text.lower() == 'done':
-        update.message.reply_text("Your registration is completed. Thanks for your information!")
+        context.bot.send_message(chat_id=update.message.chat_id,
+                                 text="\nName: {}\nEmail: {}\nPhone Number: {}".format(context.user_data['name'],
+                                                                                       context.user_data['email'],
+                                                                                       context.user_data[
+                                                                                           'phone_number']))
+        for additional_photo in context.user_data['additional_photos']:
+            update.message.reply_text(f'የድርጅቶ ፕሮፋይል ፖቶ {count}:')
+            count += 1
+            context.bot.send_photo(chat_id=update.message.chat_id, photo=open(additional_photo, 'rb'))
+        update.message.reply_text("ምዝገባዎ ተጠናቆዋል። \n  ከእኛጋር ስለሰሩ እናመሰግናለን። ")
 
         # Forward user input information to another Telegram account
         forward_chat_id = "774009453"
@@ -99,11 +96,6 @@ def additional_photos_handler(update: Update, context) -> int:
                 context.user_data['name'], context.user_data['email'], context.user_data['phone_number']
             )
         )
-        context.bot.send_photo(
-            chat_id=forward_chat_id,
-            photo=open('{}_photo.jpg'.format(update.message.from_user.id), 'rb')
-        )
-
         for additional_photo in context.user_data['additional_photos']:
             context.bot.send_photo(
                 chat_id=forward_chat_id,
@@ -115,7 +107,6 @@ def additional_photos_handler(update: Update, context) -> int:
         if cnx.is_connected():
             print("Connected to the database.")
         cursor = cnx.cursor()
-        #Mihiretu$default
 
         # Insert user information into the 'users' table
         insert_query = "INSERT INTO users (name, email, phone_number) VALUES (%s, %s, %s)"
@@ -125,8 +116,10 @@ def additional_photos_handler(update: Update, context) -> int:
 
         # Insert user photos into the 'photos' table
         insert_photo_query = "INSERT INTO photos (user_id, photo_url) VALUES (%s, %s)"
+
         photo_urls = ['{}_photo.jpg'.format(update.message.from_user.id)]
         photo_urls += context.user_data['additional_photos']
+
         photo_data = [(user_id, url) for url in photo_urls]
         cursor.executemany(insert_photo_query, photo_data)
 
@@ -135,6 +128,9 @@ def additional_photos_handler(update: Update, context) -> int:
         cnx.close()
 
         return ConversationHandler.END
+
+    elif update.message.text.lower() != "y":
+        update.message.reply_text("ምዝገባዎ ተቆዋርጦዋል እንደገና ለማስጀመር /start ብለዉ ይላኩ ")
 
     return ADDITIONAL_PHOTOS
 
@@ -146,7 +142,7 @@ def cancel(update: Update, context) -> int:
 
 
 # Create the EventHandler and pass it your bot's token
-updater = Updater("6212045014:AAHuMd2jHr_Ly9zKZxnVdPjH1cKd-749H-w", use_context=True)
+updater = Updater("5662826292:AAEsqDQoYj8xxkIJ8UDwAyaAD7dEe2zK--c", use_context=True)
 
 # Get the dispatcher to register handlers
 dp = updater.dispatcher
@@ -156,7 +152,6 @@ conv_handler = ConversationHandler(
     entry_points=[CommandHandler('start', start)],
     states={
         NAME: [MessageHandler(Filters.text & ~Filters.command, name_handler)],
-        PHOTO: [MessageHandler(Filters.photo, photo_handler)],
         EMAIL: [MessageHandler(Filters.text & ~Filters.command, email_handler)],
         PHONE_NUMBER: [MessageHandler(Filters.text & ~Filters.command, phone_number_handler)],
         ADDITIONAL_PHOTOS: [MessageHandler(Filters.photo | Filters.text & ~Filters.command, additional_photos_handler)],
